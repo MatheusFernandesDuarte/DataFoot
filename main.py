85de9b2328219a3b3b631e2e62ee4372df557329
Matheus <matheus@creditosc.com.br>
2025-01-29 23:06:58 -0300

    feat: add models and initial API structure

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
-import pandas as pd
+from flask import Flask, jsonify
+from src.repositories.db import db
+from src.utils.config import Config
 
-from datetime import datetime
+from src.modules.team.team_model import Team
+from src.modules.championship.championship_model import Championship
+from src.modules.player.player_model import Player
 
-from src.utils.selenium import Selenium
-from src.utils.accesses import AccessingURLs
-from src.utils.copying_game_info import ExtractingGameInfos
-from src.utils.copying_championship_info import ExtractingChampionshipInfos
+from src.modules.models.associations import championship_team_association
 
-def getting_data():
-    selenium = Selenium()
-    AccessingURLs(selenium.driver).join_championship()
-    
-    games_df = ExtractingChampionshipInfos(selenium.driver).get_dataframe()
-    games_df['status'] = games_df['status'].astype(str)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-    all_game_stats = {}
+    db.init_app(app)
 
-    for index, row in games_df.iterrows():
-        print(f"Processando jogo {index}: status={row['status']}")
+    with app.app_context():
+        db.metadata.clear()
+        db.create_all()
 
-        if row['status'].strip().lower() != 'ft':
-            print(f"Pulado: {index}, com status {row['status']}")
-            continue
-        
-        game_url = row['url']
-        selenium.driver.get(game_url)
-        
-        game_stats_df = ExtractingGameInfos(selenium.driver).get_dataframe()
+    @app.route("/")
+    def home() -> dict[str, str]:
+        return jsonify({"message": "Hello World"}), 200
 
-        if isinstance(game_stats_df, str):
-            raise ValueError(f"Erro: `copy_text()` retornou uma string ao invés de um DataFrame. Verifique a função `ExtractingGameInfos.copy_text()`.\nSaída: {game_stats_df}")
+    return app
 
-        game_stats = {}
-
-        for _, stat_row in game_stats_df.iterrows():
-            stat_name = stat_row["Stats"].lower().replace(" ", "_")
-            game_stats[f"home_{stat_name}"] = stat_row["Home"]
-            game_stats[f"away_{stat_name}"] = stat_row["Away"]
-
-        all_game_stats[row["url"]] = game_stats
-    
-    stats_df = pd.DataFrame.from_dict(all_game_stats, orient="index").reset_index().rename(columns={"index": "url"})
-
-    final_df = games_df.merge(stats_df, on="url", how="left")
-    print('salvando csv')
-    final_df.to_csv("jogos_com_estatisticas.csv", index=False)
-    print('csv salvo')
-
-getting_data()
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=5000)
