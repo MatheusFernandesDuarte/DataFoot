78f39a5e4bb90c35d40f7c47c855a34a0ec5cc22
Matheus <matheus@creditosc.com.br>
2025-01-29 23:03:44 -0300

    feat: adding aplication's global configs (like binds and uri)

---
+++ src/utils/config.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    """Application's global configurations."""
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_BINDS = {
+        'cards': os.getenv('BIND_CARD'),
+        'championships': os.getenv('BIND_CHAMPIONSHIP'),
+        'games': os.getenv('BIND_GAME'),
+        'games_stats': os.getenv('BIND_GAME_STATS'),
+        'goals': os.getenv('BIND_GOAL'),
+        'players': os.getenv('BIND_PLAYER'),
+        'rankings': os.getenv('BIND_RANKING'),
+        'teams': os.getenv('BIND_TEAM'),
+    }
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+print("DATABASE_URI:", Config.SQLALCHEMY_DATABASE_URI)
+print("BINDS:", Config.SQLALCHEMY_BINDS)
